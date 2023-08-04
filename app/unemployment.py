55391708c2d3662218cd40609e40f5216b2d361e
cleveland-bob <140764321+cleveland-bob@users.noreply.github.com>
2023-08-03 20:55:18 -0400

    Fixed Version

--- app/unemployment.py
+++ app/unemployment.py
@@ -1,19 +1,17 @@
-#from getpass import getpass
-#
-#API_KEY = getpass("Please input your AlphaVantage API Key: ")
 
 import os
-from dotenv import load_dotenv
-import requests
 import json
 from pprint import pprint
 from statistics import mean
-from plotly.express import line
 
+from dotenv import load_dotenv
+import requests
+from plotly.express import line
 
 load_dotenv() #> invoking this function loads contents of the ".env" file into the script's environment...
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
 
 
 def format_pct(my_number):
@@ -28,12 +26,11 @@ def format_pct(my_number):
 
 
 
-if __name__ == "__main__":
-
-    # only run the code below if we run this file from the command line
-    # otherwise if we are just importing some functions from this file
-    # don't run this code
-
+def fetch_unemployment_data():
+    """Fetches unemployment data from the AlphaVantage API.
+        Returns data as a list of dictionaries, where each represents the unemployment rate for a given month.
+        Formats rates as floats.
+    """
 
     request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
 
@@ -43,8 +40,24 @@ if __name__ == "__main__":
     print(type(parsed_response))
     pprint(parsed_response)
 
-
     data = parsed_response["data"]
+    #return data
+
+    # we could return the raw data, but the values are strings,
+    # so let's convert them to floats (more usable) before returning
+    for d in data:
+        d["value"] = float(d["value"]) # this is mutating and will overwrite the data
+
+    return data
+
+
+
+
+if __name__ == "__main__":
+
+
+
+    data = fetch_unemployment_data()
 
 
     # Challenge A
@@ -55,25 +68,19 @@ if __name__ == "__main__":
     print("-------------------------")
     print("LATEST UNEMPLOYMENT RATE:")
     #print(data[0])
-    #print(f"{data[0]['value']}%", "as of", data[0]["date"])
-    print(format_pct(float(data[0]['value'])), "as of", data[0]["date"])
-
-
-
+    print(f"{format_pct(data[0]['value'])}", "as of", data[0]["date"])
 
     # Challenge B
     #
     # What is the average unemployment rate for all months during this calendar year?
     # ... How many months does this cover?
 
-
     this_year = [d for d in data if "2023-" in d["date"]]
 
-    rates_this_year = [float(d["value"]) for d in this_year]
+    rates_this_year = [d["value"] for d in this_year]
     #print(rates_this_year)
 
     print("-------------------------")
-    #print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
     print("AVG UNEMPLOYMENT THIS YEAR:", f"{format_pct(mean(rates_this_year))}")
     print("NO MONTHS:", len(this_year))
 
@@ -82,16 +89,8 @@ if __name__ == "__main__":
     #
     # Plot a line chart of unemployment rates over time.
 
-
     dates = [d["date"] for d in data]
-    rates = [float(d["value"]) for d in data]
+    rates = [d["value"] for d in data]
 
     fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", labels= {"x": "Month", "y": "Unemployment Rate"})
     fig.show()
-
-
-
-
-
-
-
